ed2908b22a8cf9aff38f2a2bbd74570dfcc5c88f
Argustor <renatoplatz11@outlook.com>
2023-03-18 17:25:30 -0300

    Adicionando o segundo exercicio

---
+++ EX.02/EX.02/src/Dados.java
@@ -0,0 +1,13 @@
+public class Dados {
+    public static void TiposDeDados() {
+        System.out.println("Tipos de dados em Java:");
+        System.out.println("byte");
+        System.out.println("short");
+        System.out.println("int");
+        System.out.println("long");
+        System.out.println("float");
+        System.out.println("double");
+        System.out.println("char");
+        System.out.println("boolean");
+    }
+}
---
+++ EX.02/EX.02/src/Variaveis.java
@@ -0,0 +1,20 @@
+public class Variaveis {
+    public static void TiposVariaveis(){
+        System.out.println("Exemplos de variáveis em Java:");
+        // int
+        int idade = 10;
+        System.out.printf("%d é um tipo int.\n", idade);
+        // double
+        double preco = 10.99;
+        System.out.printf("%f é um tipo float\n",preco);
+        // char
+        char letra = 'a';
+        System.out.printf("%c é um tipo char\n", letra);
+        // boolean
+        boolean verdadeiro = true;
+        System.out.println("É um tipo booleano:" + verdadeiro);
+        // String
+        String nome = "João";
+        System.out.printf("%s é um tipo String\n",nome);
+    }
+}
