b4beb3a9accb46cd1529c8bb2c18c27621e162c1
Argustor <renatoplatz11@outlook.com>
2023-03-21 10:10:56 -0300

    Adicionando o quinto exercicio

---
+++ EX.05/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Disciplinas.imprimirDisciplinas();
+    }
+
+    public class Disciplinas{
+        public static void imprimirDisciplinas(){
+            System.out.println("Disciplinas do 2° ano de Engenharia de Software:");
+            System.out.println("Algoritmos e Estrutura de Dados II");
+            System.out.println("Análise e Projeto de Sistemas");
+            System.out.println("Arquitetura de Computadores");
+            System.out.println("Banco de Dados II");
+            System.out.println("Cálculo");
+            System.out.println("Interação Humano-Computador");
+            System.out.println("Projeto Integrador I");
+            System.out.println("Empreeendedorismo");
+            System.out.println("Programação Orientada a Objetos");
+        }
+    }
+}
