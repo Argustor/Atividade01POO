d141c2cade131608eef4127bcdbaa2160b99844f
Argustor <renatoplatz11@outlook.com>
2023-03-21 09:55:44 -0300

    Adicionando o terceiro exercicio

---
+++ EX.03/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite um número inteiro: ");
+        int numero = sc.nextInt();
+
+        int resultado = Soma.somar(numero);
+        System.out.println("A soma de 0 até " + numero + " é: " + resultado);
+
+        sc.close();
+    }
+}
---
+++ EX.03/src/Soma.java
@@ -0,0 +1,11 @@
+public class Soma {
+    public static int somar(int numero){
+        int soma = 0;
+        for (int i = 1; i <= numero; i++) {
+            soma += i;
+
+        }
+        return soma;
+    }
+    }
+
